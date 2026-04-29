b2f5712ee95a33b33fbc192f2e59df073a7d2553
Ibrahim Ibrahim <eebrahiim04@gmail.com>
2026-04-29 13:30:40 +0100

    feat: add bunnies array assignment

---
+++ assignment-02/index.js
@@ -0,0 +1,12 @@
+let bunnies = ["Willow", "Artemis", "Snowball", "Blueberry", "Lucy", "Hopper"];
+
+// Add 'Mario' to the end of the array
+bunnies.push("Mario");
+
+// Remove 'Lucy' from the array
+bunnies.splice(4, 1);
+
+// Add 'Luigi to the begining of the array
+bunnies.unshift("Luigi");
+
+console.log(bunnies);
